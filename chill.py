c7f31643537c7b84eb7917e45653b127b7399186
Henry Garant <henryrgarant@outlook.com>
2019-07-14 13:06:08 -0400

    Add time to output csv

--- chill.py
+++ chill.py
@@ -1,20 +1,26 @@
 from w1thermsensor import W1ThermSensor
 import threading
+import datetime
 import sys
 
 def read_temp():
-  threading.Timer(2.0, read_temp).start()
+  threading.Timer(seconds, read_temp).start()
   temp_in_celsius = sensor.get_temperature()
   temp_in_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
+
   print("---------------------")
   print(temp_in_celsius)
   print(temp_in_fahrenheit)
   print("---------------------")
+  
+  date = datetime.datetime.now()
+  date_time = datetime.datetime.strftime(date, '%H:%M:%S')
+  data = date_time + "," + str(temp_in_celsius) + "\n"
   with open(file_name, 'a') as file:
-    file.write(str(temp_in_celsius) + ",")
+    file.write(data)
 
 if len(sys.argv) < 2:
-  print("Give me a file name argument")
+  print("Give me a file name argument i.e. 'python chill.py test.csv'")
   sys.exit()
 elif ".csv" not in sys.argv[1]:
   print("Nedd a .csv brah")
