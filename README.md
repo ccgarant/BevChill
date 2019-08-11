5147a4f7a1b3c16009ec1b1f2122178cad104b68
Henri93 <henryrgarant@gmail.com>
2019-08-11 14:12:35 -0400

    server gets own ip

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
 # BevChill
 
-Open terminal
+Open terminal on laptop
 
 ### Start server
 ```python
 cd server
 python server.py
 ```
-This will attempt to start a server at http://192.168.1.180:5000/
 
-_If you get an error that it cannot start at this location run_
+Look for the line 
 ```python
-ifconfig
+MY IP ADDRESS:
 ```
-_And look for an ip that starts with 192.168_
-_Copy this and replace '192.168.1.180' in server.py and chill.py_ 
+
+#####Copy the IP ADDRESS
 
 
 ### Start client
 Open ANOTHER terminal with first still running
 
+_SSH into your pi_
+
 Start client to collect and send data
 ```python
 cd client
-python chill.py [type_in_test_file.csv]
+python chill.py [server_ip] [type_in_test_file.csv]
 ```
 
-Open http://192.168.1.180:5000/ on web to view dashboard
-
-upgrades
+Open http://\*server_ip\*:5000/ on web to view dashboard
--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ Aim is to create a web app that is constantly updated with temperatures sent by 
 @author=henry garant
 """
 
+import socket
 from flask_socketio import SocketIO
 from flask import Flask, render_template
 
@@ -24,7 +25,6 @@ def index():
 
 @socketio.on('connect')
 def test_connect():
-    # need visibility of the global thread object
     print('Client connected')
 
 @socketio.on('disconnect')
@@ -38,4 +38,10 @@ def test_data(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="192.168.1.180", port=5000)
+	#get local ip address of this computer to use for running the server
+	ip_address = socket.gethostbyname(socket.gethostname())
+	print("\n\n-----------------------------------------------")
+	print("SERVER IP ADDRESS: " + ip_address)
+	print("-----------------------------------------------\n\n")
+
+	socketio.run(app, host=ip_address, port=5000)
